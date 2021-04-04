c71554eeea4ddf921b9ba24d6ecdc66c4440644c
Levbitz <maxinosamx@gmail.com>
2021-04-04 08:58:29 +0530

    last but not Least

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -5,13 +5,6 @@ import "firebase/firestore";
 
 const app = firebase.initializeApp({
   //copy and paste your firebase confing snippet and your good to go
-
-  apiKey: "AIzaSyAGpVh8iY3ivH2Qywof0303Vwyw3jZvQ8o",
-  authDomain: "file-upload-248e4.firebaseapp.com",
-  projectId: "file-upload-248e4",
-  storageBucket: "file-upload-248e4.appspot.com",
-  messagingSenderId: "436994981297",
-  appId: "1:436994981297:web:bf96692cb5c656711e5190",
 });
 
 const fbStorage = app.storage();
